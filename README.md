c4e49a98c3e144498e4cc9a7af583ec664f97d79
Waite <Waite.Ryan.M@dol.gov>
2016-04-21 15:31:21 -0400

    Initial Code

---
+++ README.md
@@ -0,0 +1,12 @@
+#Forum
+
+http://ryanwaite28.github.io/forum/index.html
+
+Using Angular and jQuery To Create a Forum Site!
+Demonstrates the CRUD Methology (Create | Read | Update | Destroy)
+
+#How To use
+
+Input Text Into The Fields Under the Post Section.
+The Post will Appear Under Feed. You Can Edit by Clicking on the Pencil
+or Delete by Clicking on the Trash Can.
---
+++ script.js
@@ -0,0 +1,107 @@
+// Main Angular Application
+var App = angular.module("myApp", []);
+
+// Master Angular Controller
+App.controller('masterCtrl', function($scope) {
+
+	$scope.feed = [];
+
+	$scope.addPost = function() {
+
+		// console.log('Adding Post...');
+
+		var userName = $('#newpost-username').val();
+		var textBody = $('#newpost-textbody').val();
+		var date = Date();
+
+		if( userName == '' || textBody == '' ) {
+			$scope.msg1 = 'Please fill in both fields.';
+			setTimeout( function() {
+				$scope.msg1 = '';
+				$scope.$apply();
+			} , 3000 );
+			return;
+		}
+
+		$scope.feed.push({
+			userName : userName,
+			textBody : textBody,
+			postDate : date,
+			lastEdited : 'No Prior Edits'
+		})
+
+		$('#newpost-username').val('');
+		$('#newpost-textbody').val('');
+
+		// console.log('Post Added.');
+
+	}
+
+	$scope.openEditor = function(post) {
+
+		document.getElementById("edit-modal").showModal();
+
+		$('#editpost-textbody').val(post.textBody);
+
+		$scope.editPost = function() {
+
+			var editedText = $('#editpost-textbody').val();
+
+			if( editedText == '' ) {
+				$scope.msg2 = 'Please fill in the field with text.';
+				setTimeout( function() {
+					$scope.msg2 = '';
+					$scope.$apply();
+				} , 3000 );
+				return;
+			}
+
+			post.textBody = editedText;
+			post.lastEdited = Date();
+
+			document.getElementById("edit-modal").close();
+
+			return post;
+
+		}
+
+	}
+
+	$scope.closeEditPost = function() {
+
+		document.getElementById("edit-modal").close();
+		//$('#edit-modal').attr(open , false);
+
+	}
+
+	$scope.deletePost = function(post) {
+
+		// console.log(post);
+		// console.log('Deleting Post...');
+
+		var index = $scope.feed.indexOf(post);
+		console.log(index);
+
+		if( index != -1 ) {
+
+			var ask = confirm('Are You Sure You Want To Delete This Post?');
+
+			if( ask == true ) {
+				$scope.feed.splice(index , 1);
+			}
+			if( ask == false ) {
+				return;
+			}
+
+		}
+		if( index == -1 ) {
+
+			return;
+
+		}
+
+		// console.log('Post Deleted.');
+
+	}
+
+});
